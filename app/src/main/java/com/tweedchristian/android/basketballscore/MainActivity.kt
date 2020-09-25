68401f334fd7491f51e71ada6809d9ff0bedf435
Christian Tweed <christiantweed@gmail.com>
2020-09-25 02:33:50 -0400

    Finished Activity to Activity communication, and added basic layouts for game results

--- app/src/main/java/com/tweedchristian/android/basketballscore/MainActivity.kt
+++ app/src/main/java/com/tweedchristian/android/basketballscore/MainActivity.kt
@@ -1,26 +1,20 @@
 package com.tweedchristian.android.basketballscore
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.LinearLayout
-import android.widget.TextView
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 
 private const val TAG = "MainActivity"
 private const val TEAM_ONE_INDEX = "teamOne"
 private const val TEAM_TWO_INDEX = "teamTwo"
-
-//TODO: 2nd Activity (Previous Scores)
-//TODO: 2nd Activity Layout
-//TODO: 2nd Activity Activity Manager
 
 //TODO: Fragment
 
@@ -34,11 +28,14 @@ class MainActivity : AppCompatActivity() {
     private lateinit var teamOneFreeThrowButton: Button
     private lateinit var teamTwoFreeThrowButton: Button
     private lateinit var resetButton: Button
+    private lateinit var saveButton: Button
+    private lateinit var displayButton: Button
+
     private lateinit var teamOnePointsTextView: TextView
     private lateinit var teamTwoPointsTextView: TextView
-
     private lateinit var teamOneTitle: EditText
     private lateinit var teamTwoTitle: EditText
+
     private lateinit var linearLayout: LinearLayout
 
     private val basketballViewModel: BasketballViewModel by lazy {
@@ -48,24 +45,29 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        Log.i(TAG, "onCreate Called")
+
         teamButtonInit()
         val teamOneInitialPoints = savedInstanceState?.getInt(TEAM_ONE_INDEX, 0) ?: 0
         val teamTwoInitialPoints = savedInstanceState?.getInt(TEAM_TWO_INDEX, 0) ?: 0
-//        Log.i(TAG, "Team One:$teamOneInitialPoints")
-//        Log.i(TAG, "Team Two: $teamTwoInitialPoints")
         basketballViewModel.setPointsFromSavedState(teamOneInitialPoints, teamTwoInitialPoints)
         updatePoints()
     }
 
     override fun onSaveInstanceState(savedInstanceState: Bundle) {
         super.onSaveInstanceState(savedInstanceState)
-//        Log.i(TAG, "onSaveInstanceState")
-//        Log.i(TAG, "Storing point amounts: " +
-//                "${basketballViewModel.teamOneCurrentPoints}, " +
-//                "${basketballViewModel.teamTwoCurrentPoints}")
+
         savedInstanceState.putInt(TEAM_ONE_INDEX, basketballViewModel.teamOneCurrentPoints)
         savedInstanceState.putInt(TEAM_TWO_INDEX, basketballViewModel.teamTwoCurrentPoints)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        Log.d(TAG, resultCode.toString())
+        Log.d(TAG, Activity.RESULT_OK.toString())
+        if(resultCode == Activity.RESULT_OK) {
+            Log.d(TAG, "Passed Check")
+            Toast.makeText(this, R.string.gameResultsString, Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onStart() {
@@ -75,7 +77,7 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                Log.d(TAG, "pp")
+                basketballViewModel.basketBallGame.updateTeamName(true, s.toString())
             }
 
             override fun afterTextChanged(s: Editable?) {
@@ -87,14 +89,13 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                Log.d(TAG, "pp2")
+               basketballViewModel.basketBallGame.updateTeamName(false, s.toString())
             }
 
             override fun afterTextChanged(s: Editable?) {
             }
 
         }
-
         teamOneTitle.addTextChangedListener(teamOneTitleWatcher)
         teamTwoTitle.addTextChangedListener(teamTwoTitleWatcher)
     }
@@ -103,7 +104,6 @@ class MainActivity : AppCompatActivity() {
      * A function to update the displayed text to what is stored in the view model
      */
     private fun updatePoints() {
-//        Log.i(TAG, "Updating the text view")
         teamOnePointsTextView.text = basketballViewModel.teamOneCurrentPoints.toString()
         teamTwoPointsTextView.text = basketballViewModel.teamTwoCurrentPoints.toString()
     }
@@ -112,7 +112,6 @@ class MainActivity : AppCompatActivity() {
      * A function that finds all the buttons and text views and set up callbacks
      */
     private fun teamButtonInit() {
-    //    Log.i(TAG, "Initializing and loading buttons")
         //Team One Init
         teamOne3ShotButton = findViewById(R.id.teamOne3Points)
         teamOne2ShotButton = findViewById(R.id.teamOne2Points)
@@ -129,8 +128,10 @@ class MainActivity : AppCompatActivity() {
         teamTwoTitle = findViewById(R.id.teamTwoName)
         teamTwoTitle.clearFocus()
 
-        //Reset Button
+        //Utility Buttons
         resetButton = findViewById(R.id.resetButton)
+        saveButton = findViewById(R.id.saveButton)
+        displayButton = findViewById(R.id.displayButton)
 
         //Root Element
         linearLayout = findViewById(R.id.root)
@@ -170,6 +171,11 @@ class MainActivity : AppCompatActivity() {
         resetButton.setOnClickListener {
             basketballViewModel.resetPoints()
             updatePoints()
+        }
+
+        saveButton.setOnClickListener{
+            val intent = GameResults.newIntent(this@MainActivity, basketballViewModel.basketBallGame)
+            startActivityForResult(intent, 0)
         }
 
         //Hide Keyboard and Clear Focus on Click-away
